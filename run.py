d53293823af79a931e689e26d3a0b2a5d639a653
Maria Ciceri <4e4gw8bo@students.codeinstitute.net>
2024-12-04 14:38:24 +0000

    Add feedback after submitting form

--- run.py
+++ run.py
@@ -1,7 +1,12 @@
 import os
 import json
 from flask import Flask, render_template, request, flash
+if os.path.exists("env.py"):
+    import env
+
+
 app = Flask(__name__)
+app.secret_key = os.environ.get("SECRET_KEY")
 
 
 @app.route("/")
@@ -31,8 +36,8 @@ def about_member(member_name):
 @app.route("/contact", methods=["GET", "POST"])
 def contact():
     if request.method == "POST":
-        print(request.form.get("name"))
-        print(request.form["email"])
+        flash("Thanks {}, we have received your message!".format(
+            request.form["name"]))
     return render_template("contact.html", page_title="Contact")
 
 
